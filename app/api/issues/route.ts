36eab5a9e40f2e08f8f60e19e677ed3676d543d2
sandunrmst <sandunalugc@gmail.com>
2023-11-13 15:25:44 +0530

    build issue API for create issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import {z} from 'zod'
+import prisma from "@/prisma/migrations/client";
+
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+})
+
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body)
+
+    //check validation help with zod
+    if(!validation.success)
+        return NextResponse.json(validation.error.errors, {status:400})
+
+        //create new issue in db help with prisma client.ts
+        const newIssue = await prisma.issue.create({
+            data: { title: body.title, description: body.description}
+        })
+
+    return NextResponse.json(newIssue, {status: 201})
+
+}
