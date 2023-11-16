f84c001e45d94282566ed02277a9dfba4df5df91
sandunrmst <sandunalugc@gmail.com>
2023-11-16 15:36:09 +0530

    show single issue details

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import IssueButton from "./issueButton";
+import IssueButton from "./IssueButton";
 
 const LoadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5, 6];
